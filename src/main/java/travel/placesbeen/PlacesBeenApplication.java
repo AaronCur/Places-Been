ff5fdc842f3f82d3bf263b50c15064ff9630a91a
AaronCur <aaron.m.curry84@gmail.com>
2026-05-10 20:26:49 +0100

    Starter filed

---
+++ src/main/java/travel/placesbeen/PlacesBeenApplication.java
@@ -0,0 +1,13 @@
+package travel.placesbeen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesBeenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacesBeenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/travel/placesbeen/PlacesBeenApplicationTests.java
@@ -0,0 +1,13 @@
+package travel.placesbeen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesBeenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
