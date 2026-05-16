075650c262be0f68a9335910c641e6abbb689ddf
AaronCur <aaron.m.curry84@gmail.com>
2026-05-16 18:40:25 +0100

    Adding spring boot 4 api version support

--- src/main/java/travel/placesbeen/city/CityController.java
+++ src/main/java/travel/placesbeen/city/CityController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/cities")
+@RequestMapping("/api/cities")
 @Tag( name = "City API", description = "API for managing cities")
 public class CityController {
     private final CityService cityService;
@@ -20,17 +20,17 @@ public class CityController {
         this.cityService = cityService;
     }
 
-    @GetMapping
+    @GetMapping(version = "1")
     public ResponseEntity<List<CityResponse>> getAllCities() {
         return ResponseEntity.ok(cityService.getAllCities());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", version = "1")
     public ResponseEntity<CityResponse> getCityById(@PathVariable Long id) {
         return ResponseEntity.ok(new CityResponse(1L, "New York", 40.7128, -74.0060, "USA", "https://flagcdn.com/w40.png"));
     }
 
-    @PostMapping
+    @PostMapping(version = "1")
     public ResponseEntity<CityResponse> addCity(@Valid @RequestBody CityRequest cityRequest) {
         CityResponse newCity = cityService.addCity(cityRequest);
         URI location = ServletUriComponentsBuilder
@@ -41,12 +41,12 @@ public class CityController {
         return ResponseEntity.created(location).body(newCity);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", version = "1")
     public ResponseEntity<CityResponse> updateCity(@PathVariable Long id, @Valid @RequestBody CityRequest cityRequest) {
         return ResponseEntity.ok(new CityResponse(1L, "New York", 40.7128, -74.0060, "USA", "https://flagcdn.com/w40.png"));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}", version = "1")
     public ResponseEntity<Void> deleteCity(@PathVariable Long id) {
         return ResponseEntity.noContent().build();
     }
--- src/main/java/travel/placesbeen/country/CountryController.java
+++ src/main/java/travel/placesbeen/country/CountryController.java
@@ -1,11 +1,17 @@
 package travel.placesbeen.country;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping( "/api/v1/countries")
+@RequestMapping( "/api/countries")
 @Tag( name = "Country API", description = "API for managing countries")
 public class CountryController {
+
+    @GetMapping(version = "1")
+    public String getAllCountries() {
+        return "All countries";
+    }
 }
